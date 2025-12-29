28d7a7ae6cf5dce67f5e64f934e3bcf6b35ac361
arthcrmx <arthur.cremieux@gmail.com>
2025-12-29 20:31:15 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,42 +1,39 @@
-/* CONFIGURATION DES 30 JOURS 
-    Tu peux modifier ce fichier à tout moment sur GitHub, 
-    et le site se mettra à jour.
-*/
-
+/* CONFIG GENEREE */
 const calendarData = [
     {
-        day: 1,
-        gameType: "none", // Pas de jeu, ouverture directe
-        reward: {
-            type: "text",
-            content: "Bienvenue mon amour. C'est le début du compte à rebours. Courage, je suis fier de toi."
+        "day": 1,
+        "gameType": "none",
+        "reward": {
+            "type": "text",
+            "content": "Pour ce premier jour, un petit cadeau pour fêter ce nouvel an.",
+            "text": "je reviens dans 5 minutes !"
+        },
+        "question": "",
+        "answer": ""
+    },
+    {
+        "day": 2,
+        "gameType": "quiz",
+        "question": "Quel est ma couleur préférée ? (je change tout le temps donc bon courage)",
+        "answer": "Rose",
+        "reward": {
+            "type": "coupon",
+            "content": "10 minutes de massage !",
+            "text": "Pour te déstresser avant ton voyage retour."
         }
     },
     {
-        day: 2,
-        gameType: "quiz",
-        question: "Quel est le nom du restaurant de notre premier date ?",
-        answer: "Luigi", // La réponse attendue (insensible aux majuscules)
-        reward: {
-            type: "image",
-            content: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?w=600",
-            text: "C'était magique. J'ai hâte d'y retourner avec toi."
+        "day": 3,
+        "gameType": "code",
+        "question": "mon code de téléphone",
+        "answer": "050206",
+        "reward": {
+            "type": "video",
+            "content": "https://youtu.be/HVQSbgG69Sc?si=vWDEELTCtUJ-q_W8",
+            "text": "lol, il va pas y avoir des trucs gratuits tous les jours heinn"
         }
-    },
-    {
-        day: 3,
-        gameType: "code",
-        question: "Quel est le code secret ? (Indice : Le jour de mon retour)",
-        answer: "3001", 
-        reward: {
-            type: "video",
-            content: "ID_VIDEO_YOUTUBE", // Mets l'ID youtube ici
-            text: "Une petite chanson pour te donner du courage."
-        }
-    },
-    // ... Copie colle les blocs pour faire les 30 jours !
+    }
 ];
 
-// Configuration Date
-const START_MONTH = 0; // 0 = Janvier
+const START_MONTH = 0;
 const START_YEAR = 2026;
